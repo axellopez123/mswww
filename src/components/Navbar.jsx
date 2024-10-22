25ce6389e7ce004604f0706bddbaa2c829cd2311
Ballardo Axel López Martinez <ballardo.lopez@jalisco.gob.mx>
2024-10-22 16:26:33 -0600

    Menu que se abre con boton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,45 @@
-// src/components/Navbar.jsx
-import React from 'react';
-import Button from '@mui/material/Button';
-import { useDrawerContext } from '../contexts/DrawerContext';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useDrawerContext } from "../contexts/DrawerContext";
 
 export default function Navbar() {
   const { toggleDrawer } = useDrawerContext();
 
   return (
-    <nav>
-      <Button onClick={() => toggleDrawer(true)}>Open Sidebar</Button>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <div className="rounded-full bg-red-800 py-3 absolute -left-4">
+            <Button onClick={() => toggleDrawer(true)}>
+              <p className="text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+                  />
+                </svg>
+              </p>
+            </Button>
+          </div>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { DrawerProvider } from '../contexts/DrawerContext';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
-
 export default function MainLayout({ children }) {
   return (
     <DrawerProvider>
       <div>
+
         <Navbar />
         <Sidebar />
         <main>{children}</main>
