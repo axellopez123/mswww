2e2066ace12298992bf2e377c6a3b2ea93f155e8
Ballardo Axel López Martinez <ballardo.lopez@jalisco.gob.mx>
2024-10-30 17:03:37 -0600

    Theme button

--- src/components/ThemeToggleButton.jsx
+++ src/components/ThemeToggleButton.jsx
@@ -25,9 +25,41 @@ const ThemeToggleButton = () => {
   return (
     <button
       onClick={toggleTheme}
-      className="p-2 bg-blue-500 text-white rounded"
+      className={`${
+        isDarkMode ? "bg-blue-900 text-white border-white" : "bg-white text-yellow-800 font-semibold border-black"
+      }  border-2 font-bold py-3 px-3 rounded rounded-full transition-colors duration-300 focus:outline-none shadow shadow-2xl border-double`}
     >
-      Cambiar a {isDarkMode ? "Tema Claro" : "Tema Oscuro"}
+      {isDarkMode ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2.5"
+          stroke="currentColor"
+          class="size-5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2.5"
+          stroke="currentColor"
+          class="size-5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+          />
+        </svg>
+      )}
     </button>
   );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import Box from "@mui/material/Box";
 import ParallaxScene from '../components/ParallaxScene';
 import WavyBackground  from '../components/WavyBackground'
+import Banner from '../components/Banner'
 export default function Home() {
   return (
     <Box className="flex-1 overflow-y-auto p-6 bg-gray-100">
       <div className="h-full flex items-center justify-center text-gray-700">
-        <p className="text-lg">Contenido principal de la página.</p>
        <WavyBackground/>
       </div>
     </Box>
